db1b6b9a1fa24d6ba84fc49cdd3256994f01c03c
Andrew Ardill <andrew.ardill@gmail.com>
2011-02-22 00:56:05 +1100

    The wrong event signature was being used.

--- src/main/java/net/gamesketch/bukkit/motd/LoginListener.java
+++ src/main/java/net/gamesketch/bukkit/motd/LoginListener.java
@@ -1,13 +1,14 @@
 package net.gamesketch.bukkit.motd;
 
 import org.bukkit.entity.Player;
+import org.bukkit.event.player.PlayerEvent;
 import org.bukkit.event.player.PlayerListener;
-import org.bukkit.event.player.PlayerLoginEvent;
 
 public class LoginListener extends PlayerListener {
 
-    public void onPlayerJoin(PlayerLoginEvent event) {
+    public void onPlayerJoin(PlayerEvent event) {
         Player player = event.getPlayer();
         player.sendMessage("Welcome!");
     }
+
 }
