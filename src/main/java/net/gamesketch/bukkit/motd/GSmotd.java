b2b5055840a3db72f0671847bccdcbb05700fe6a
Streammz <robbygroot@quicknet.nl>
2011-02-21 13:07:48 +0100

    Probably wrong xD

--- src/main/java/net/gamesketch/bukkit/motd/GSmotd.java
+++ src/main/java/net/gamesketch/bukkit/motd/GSmotd.java
@@ -9,6 +9,13 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.PluginLoader;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.entity.Player;
+import org.bukkit.event.player.PlayerEvent;
+import org.bukkit.event.player.PlayerListener;
+import org.bukkit.event.player.PlayerLoginEvent;
+import org.bukkit.plugin.PluginManager;
+import org.bukkit.event.Event;
+import org.bukkit.event.Event.Priority;
 
 /**
  * GameSketch Network message of the day
@@ -17,7 +24,8 @@ import org.bukkit.plugin.java.JavaPlugin;
  * @author Cogito
  */
 public class GSmotd extends JavaPlugin {
-
+	private final PlayerListener playerListener = new PlayerListener();
+	
     public GSmotd(PluginLoader pluginLoader, Server instance, PluginDescriptionFile desc, File folder, File plugin, ClassLoader cLoader) {
         super(pluginLoader, instance, desc, folder, plugin, cLoader);
     }
@@ -29,6 +37,8 @@ public class GSmotd extends JavaPlugin {
     public void onEnable() {       
         // EXAMPLE: Custom code, here we just output some info so we can check all is well
         PluginDescriptionFile pdfFile = this.getDescription();
+        PluginManager pm = getServer().getPluginManager();
+        pm.registerEvent(Event.Type.PLAYER_JOIN, playerListener, Priority.Normal, this);
         System.out.println( pdfFile.getName() + " version " + pdfFile.getVersion() + " is enabled!" );
     }
 
@@ -37,8 +47,12 @@ public class GSmotd extends JavaPlugin {
         String commandName = command.getName().toLowerCase();
 
         if (commandName.equals("setmotd")) {
-            // TODO set the motd
         }
         return false;
+    }
+    
+    public void onPlayerJoin(PlayerLoginEvent event) {
+    	Player player = event.getPlayer();
+    	player.sendMessage("Welcome!");
     }
 }
