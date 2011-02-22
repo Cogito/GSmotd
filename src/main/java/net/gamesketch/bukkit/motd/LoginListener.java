2c6bb0d00cca152514e45bbb3584ce577dbc26be
Streammz <robbygroot@quicknet.nl>
2011-02-22 14:49:13 +0100

    Added the reading of a file GSmotd.txt

--- src/main/java/net/gamesketch/bukkit/motd/LoginListener.java
+++ src/main/java/net/gamesketch/bukkit/motd/LoginListener.java
@@ -1,5 +1,9 @@
 package net.gamesketch.bukkit.motd;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 import org.bukkit.entity.Player;
 import org.bukkit.event.player.PlayerEvent;
 import org.bukkit.event.player.PlayerListener;
@@ -8,7 +12,18 @@ public class LoginListener extends PlayerListener {
 
     public void onPlayerJoin(PlayerEvent event) {
         Player player = event.getPlayer();
-        player.sendMessage("Welcome!");
-    }
+    
+       
+        try {
+            BufferedReader in = new BufferedReader(new FileReader("plugins/gsmotd/gsmotd"));
+            String str;
+            while ((str = in.readLine()) != null) {
+                player.sendMessage(str);
+            }
+            in.close();
+        } catch (IOException e) {
+        }
 
+
+    }
 }
